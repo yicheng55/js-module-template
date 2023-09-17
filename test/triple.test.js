49c8347c268cfd438eb86c98cfc22d411a3d90e8
yicheng <l5977951@gmail.com>
2023-09-17 21:26:48 +0800

    const mySquare = new Square(15); const myRectangle = new Rectangle(5, 15);

--- test/triple.test.js
+++ test/triple.test.js
@@ -1,4 +1,5 @@
-import { Triple } from "../src";
+// import { Triple } from "../src";
+import { Triple, Square, Rectangle } from "../src";
 
 test("import Triple", () => {
   expect.anything(Triple);
@@ -7,4 +8,21 @@ test("import Triple", () => {
 test("Triple.triple", () => {
   const temp = Triple.triple(5);
   expect(temp).toBe(15);
+});
+
+test("import Square", () => {
+  expect.anything(Square);
+});
+
+test("New Square", () => {
+  const mySquare = new Square(15);
+  mySquare.sayName();
+  expect(mySquare.area).toBe(mySquare.area);
+});
+
+test("New Rectangle", () => {
+  const myRectangle = new Rectangle(5, 15);
+  myRectangle.sayName();
+  console.log(myRectangle);
+  // expect(myRectangle.area).toBe(myRectangle.area);
 });
